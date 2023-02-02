a503d9e78772deac7616b9c28cd18fcac7b7fa4a
Francisco Javier Alarza <fjalarza@viewnext.com>
2023-02-02 21:10:28 +0100

    Initial Commit

---
+++ PetShelter/PetShelterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetShelterApp.swift
+//  PetShelter
+//
+//  Created by Francisco Javier Alarza Sanchez on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct PetShelterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
