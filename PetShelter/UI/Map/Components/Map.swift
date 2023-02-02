715d7192fbe795115103374acc84e1bb15d349ae
Francisco Javier Alarza <franalarza@gmail.com>
2023-02-02 21:28:11 +0100

    initial structure

---
+++ PetShelter/UI/Map/Components/Map.swift
@@ -0,0 +1,20 @@
+//
+//  Map.swift
+//  PetShelter
+//
+//  Created by Francisco Javier Alarza Sanchez on 2/2/23.
+//
+
+import SwiftUI
+
+struct Map: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Map_Previews: PreviewProvider {
+    static var previews: some View {
+        Map()
+    }
+}
