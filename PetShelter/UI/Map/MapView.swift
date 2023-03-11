cf7401c54a2a46fee77d03c774fb5fac85faf34d
Joaquín Corugedo <joaquin_coru@hotmail.com>
2023-03-11 13:23:21 +0100

    Solve issue with force unwraping closest shelter and documentation of MapView

--- PetShelter/UI/Map/MapView.swift
+++ PetShelter/UI/Map/MapView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 import Combine
 
+/// Vista que muestra mapa con la localización del usuario y lista de Shelters disponibles alrededor
 struct MapView: View {
+
     @ObservedObject var viewModel = MapViewModel()
     
     /// Variable para asignar shelter al que se hace click
@@ -39,9 +41,20 @@ struct MapView: View {
         })
         /// Modal que se presenta al intentar buscar el shelter más cercano
         .sheet(isPresented: $isClosestPresented, content: {
-            ShelterDetailModal(shelter: viewModel.closestShelter!)
-                .presentationDetents([.fraction(0.40), .large])
-                .padding(.top, 20)
+            
+            ///Condición para mostrar shelter si no es nulo el valor o texto de error por defecto
+            if let closestShelter = viewModel.closestShelter {
+                ShelterDetailModal(shelter: closestShelter)
+                    .presentationDetents([.fraction(0.40), .large])
+                    .padding(.top, 20)
+            } else {
+                Text("Error al cargar refugio")
+                    .presentationDetents([.fraction(0.40)])
+                    .font(Font.custom("Moderat-Medium",size: 18))
+                    .foregroundColor(Color("RedKiwoko"))
+                    .padding(.top, 20)
+            }
+
         })
         
         .onAppear {
